6421234eb8c9720f86cd225eca54601933328788
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-10 12:32:00 +0600

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
