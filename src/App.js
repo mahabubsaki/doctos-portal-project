4a82cee422f142ac8673057d2025105e16c8fd12
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-10 15:04:15 +0600

    all route set up complete on navbar and app js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Navbar from './components/shared/Navbar';
+import About from './components/pages/About';
+import Appointment from './components/pages/Appointment';
+import Reviews from './components/pages/Reviews';
+import Contact from './components/pages/Contact';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import NotFound from './components/pages/NotFound';
+import Footer from './components/shared/Footer';
 
 function App() {
   return (
@@ -9,7 +17,15 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/appointment" element={<Appointment></Appointment>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/contact" element={<Login></Login>}></Route>
+        <Route path="/Register" element={<Register></Register>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/appointment">Appointment</Link></li>
-                        <li><Link to="/about">Reviews</Link></li>
+                        <li><Link to="/reviews">Reviews</Link></li>
                         <li><Link to="/contact">Contact Us</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/register">Sign Up</Link></li>
