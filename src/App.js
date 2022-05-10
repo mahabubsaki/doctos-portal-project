84bcef80cb006a16462c01786139e1ae3e0e4a9a
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-10 14:58:36 +0600

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Navbar from './components/shared/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
       </Routes>
