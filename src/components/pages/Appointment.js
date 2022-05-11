c99da4231dc2751876ca6140c7ce9b953f889a69
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-11 20:45:47 +0600

    installed date picker and date fns

--- src/components/pages/Appointment.js
+++ src/components/pages/Appointment.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png'
 
 const Appointment = () => {
     return (
-        <div>
+        <div className="hero min-h-screen banner">
+            <div className="hero-content flex-col md:flex-row-reverse">
+                <img src={chair} alt="chair" className="sm:max-w-md max-w-xs rounded-lg shadow-2xl" />
+                <div>
 
+                </div>
+            </div>
         </div>
     );
 };
