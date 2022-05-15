39800d6de0b518d8546608f46e7c0638bc0d3410
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-15 21:19:47 +0600

    installed react query

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
